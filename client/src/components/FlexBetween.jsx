a84e5f12ead5e89a2e5a6bff8a8ae3cac816fc00
Thapelo-git <chabathapelo1@gmail.com>
2024-08-09 10:29:23 +0200

    commit

---
+++ client/src/components/FlexBetween.jsx
@@ -0,0 +1,10 @@
+import {Box} from "@mui/icons-material"
+import styled from "@emotion/styled"
+
+const FlexBetween = styled(Box)({
+    display:"flex",
+    justifyContent:"space-between",
+    alignItems:"center"
+
+})
+export default FlexBetween;
