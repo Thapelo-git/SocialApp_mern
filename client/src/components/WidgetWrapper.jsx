426fb3c1c4befaa527cbb9ac72c3c600521fd959
Thapelo-git <chabathapelo1@gmail.com>
2024-09-13 21:36:04 +0200

    commit

--- client/src/components/WidgetWrapper.jsx
+++ client/src/components/WidgetWrapper.jsx
@@ -1,6 +1,6 @@
 
 import { Box } from '@mui/material'
-import styled from '@mui/material'
+import styled from '@emotion/styled';
 
 const WidgetWrapper = styled(Box)(({theme})=>({
     padding:"1.5rem 1.5rem 0.75rem 1.5rem",
--- client/src/scenes/loginPage/Form.jsx
+++ client/src/scenes/loginPage/Form.jsx
@@ -74,7 +74,7 @@ const Form = () => {
 
     const login = async (values,onSubmitProps)=>{
         const loggedInResponse = await fetch(
-            "http://localhost:3001/auth/register",{
+            "http://localhost:3001/auth/login",{
                 method:"POST",
                 headers:{"Content-Type":"application/json"},
                 body:JSON.stringify(values),
