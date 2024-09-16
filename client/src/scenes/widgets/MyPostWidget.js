a773b6688a7742e2ee004a22b2114bf14c05980a
Thapelo-git <chabathapelo1@gmail.com>
2024-09-16 12:55:22 +0200

    commit

--- client/src/scenes/widgets/MyPostWidget.js
+++ client/src/scenes/widgets/MyPostWidget.js
@@ -68,6 +68,7 @@ const MyPostWidget = ({picturePath}) => {
                   p="1rem"
                   >
                        <Dropzone
+                      
                             acceptedFiles=".jpg,.jpeg,.png"
                             multiple={false}
                             onDrop={(acceptedFiles)=>
@@ -118,6 +119,39 @@ const MyPostWidget = ({picturePath}) => {
             Image
           </Typography>
         </FlexBetween>
+        {isNonMobileScreens ? (
+          <>
+            <FlexBetween gap="0.25rem">
+              <GifBoxOutlined sx={{ color: mediumMain }} />
+              <Typography color={mediumMain}>Clip</Typography>
+            </FlexBetween>
+
+            <FlexBetween gap="0.25rem">
+              <AttachFileOutlined sx={{ color: mediumMain }} />
+              <Typography color={mediumMain}>Attachment</Typography>
+            </FlexBetween>
+
+            <FlexBetween gap="0.25rem">
+              <MicOutlined sx={{ color: mediumMain }} />
+              <Typography color={mediumMain}>Audio</Typography>
+            </FlexBetween>
+          </>
+        ) : (
+          <FlexBetween gap="0.25rem">
+            <MoreHorizOutlined sx={{ color: mediumMain }} />
+          </FlexBetween>
+        )}
+         <Button
+          disabled={!post}
+          onClick={handlePost}
+          sx={{
+            color: palette.background.alt,
+            backgroundColor: palette.primary.main,
+            borderRadius: "3rem",
+          }}
+        >
+          POST
+        </Button>
         </FlexBetween>
     </WidgetWrapper>
   )
